e0c21721b6f3f2da7dcfff27899b34b4cee7fae4
LIUWEICHENG\a0966 <a0966891512@gmail.com>
2023-09-14 00:00:56 +0900

    W

--- Assets/0MY/2Enemy/SpacecraftCarrier/Frighter.cs
+++ Assets/0MY/2Enemy/SpacecraftCarrier/Frighter.cs
@@ -5,10 +5,14 @@ using UnityEngine;
 public class Frighter : MonoBehaviour
 {
     [SerializeField] int frighterPower = 5;
-    [SerializeField] GameObject frightBoxs;
-    [SerializeField] Transform frightBoxTransform;
+    [SerializeField] GameObject[] frightBoxs;
+    [SerializeField] GameObject[] frightBoxs2;
+    [SerializeField] Transform[] frightBoxTransforms;
 
-
+    private void Start()
+    {
+        
+    }
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
